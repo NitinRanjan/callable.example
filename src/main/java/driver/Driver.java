318b142773d2eb022a796968e87ec204368ac98d
Nitin Ranjan <nitinranjan269@gmail.com>
2018-07-21 18:03:01 +0530

    complete code

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,20 +1,74 @@
 package driver;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import service.CallableService;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.*;
 
 public class Driver {
-    public static void main(String... args) throws Exception{
-        ExecutorService es = Executors.newFixedThreadPool(5);
-        Callable call = new CallableService();
-        Future<Integer> firstcall = es.submit(call);
-        while(!firstcall.isDone());
-
-        System.out.println(firstcall.get());
-        es.shutdown();
+    static  Logger DriverLogger = Logger.getLogger("GenericClass");
+    public static void main(String... args) {
+        PropertyConfigurator.configure("log4j.properties");
+        DriverLogger.info("\n\n\n###### Using Callable Interface ######");
+        callableUsingCallableInterface();
+        DriverLogger.info("\n\n\n###### Using Lambda Expression ######");
+        callableUsingLambda();
     }
+
+    private static void callableUsingLambda() {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Queue<Future<String>> taskQueue =  new LinkedList<Future<String>>();
+        while (taskQueue.size()<10)
+        {
+            Callable<String> call = () -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return Thread.currentThread().getName();
+            };
+            taskQueue.add(executorService.submit(call));
+        }
+        waitForAllTask2Complete(taskQueue);
+        printAllTaskResult(taskQueue);
+        executorService.shutdown();
+    }
+
+    private static void callableUsingCallableInterface() {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Queue<Future<String>> taskQueue =  new LinkedList<Future<String>>();
+        while (taskQueue.size()<10)
+            taskQueue.add(executorService.submit(new CallableService()));
+        waitForAllTask2Complete(taskQueue);
+        printAllTaskResult(taskQueue);
+        executorService.shutdown();
+    }
+
+    private static void printAllTaskResult(Queue<Future<String>> taskQueue) {
+        while (taskQueue.size()>0) {
+            try {
+                DriverLogger.info(taskQueue.poll().get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void waitForAllTask2Complete(Queue<Future<String>> taskQueue) {
+        boolean isPending = true;
+        while (isPending) {
+            isPending = true;
+            for(var task : taskQueue) {
+                isPending &= !task.isDone();
+            }
+        }
+    }
+
 }
+
